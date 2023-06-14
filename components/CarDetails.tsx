3e5213afecd57557f939f05d4ac75aba6b4f3c7d
Augusto Kalahary <AuuKalaharyKW@gmail.com>
2023-06-14 17:02:41 -0300

    Visual adjustments CarDetails component

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -36,7 +36,7 @@ const CarDetails = ({ isOpen, closeModal, car }: ICarDetails) => {
                 enter="ease-out duration-300"
                 enterFrom="opacity-0 scale-95"
                 enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
+                leave="ease-out duration-300"
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
